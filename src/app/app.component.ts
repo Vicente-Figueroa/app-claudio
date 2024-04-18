643c9f6d34ae4ab1b0695d2c2b742e52f5683b9d
Vicente Figueroa <dev.vicentefigueroa@gmail.com>
2024-04-18 17:55:25 -0400

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './main_components/sidebar/sidebar.component';
+import { OnboardingComponent } from './features/onboarding/onboarding.component';
+import { HeaderComponent } from './main_components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app-claudio';
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutmeComponent } from './features/aboutme/aboutme.component';
+import { OnboardingComponent } from './features/onboarding/onboarding.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'sobre_mi', component: AboutmeComponent },
+  { path: '**', component: OnboardingComponent }
+];
