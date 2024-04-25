c2ef17719da1d856f09fe72aec28c46e59db48b9
Vicente Figueroa <dev.vicentefigueroa@gmail.com>
2024-04-25 11:25:35 -0400

    Se quito la accion de scroll

--- src/app/features/aboutme/aboutme.component.ts
+++ src/app/features/aboutme/aboutme.component.ts
@@ -6,28 +6,10 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [],
   templateUrl: './aboutme.component.html',
-  styleUrl: './aboutme.component.css'
+  styleUrl: './aboutme.component.css',
 })
-export class AboutmeComponent {  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
-previousScrollTop = 0; // Track previous scroll position
-previousTouchY = 0; // Track previous touch position
+export class AboutmeComponent {
+  constructor() {}
 
-constructor(private router: Router) {}
-
-ngAfterViewInit() {
-  this.scrollContainer.nativeElement.addEventListener(
-    'touchmove',
-    (event: TouchEvent) => {
-      const target = event.target as HTMLElement;
-      const currentTouchY = event.touches[0].clientY; // Get Y-coordinate of first touch
-      const touchThreshold = -80; // Adjust this value for touch sensitivity
-      console.log(currentTouchY - this.previousTouchY);
-      if (currentTouchY - this.previousTouchY < touchThreshold) {
-        this.router.navigate(['portafolio']);
-      }
-
-      this.previousTouchY = currentTouchY; // Update previous touch position
-    }
-  );
-}
+  ngAfterViewInit() {}
 }
--- src/app/features/onboarding/onboarding.component.ts
+++ src/app/features/onboarding/onboarding.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-onboarding',
@@ -8,27 +7,4 @@ import { Router } from '@angular/router';
   templateUrl: './onboarding.component.html',
   styleUrl: './onboarding.component.css',
 })
-export class OnboardingComponent {
-  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
-  previousScrollTop = 0; // Track previous scroll position
-  previousTouchY = 0; // Track previous touch position
-
-  constructor(private router: Router) {}
-
-  ngAfterViewInit() {
-    this.scrollContainer.nativeElement.addEventListener(
-      'touchmove',
-      (event: TouchEvent) => {
-        const target = event.target as HTMLElement;
-        const currentTouchY = event.touches[0].clientY; // Get Y-coordinate of first touch
-        const touchThreshold = -80; // Adjust this value for touch sensitivity
-        console.log(currentTouchY - this.previousTouchY);
-        if (currentTouchY - this.previousTouchY < touchThreshold) {
-          this.router.navigate(['sobre_mi']);
-        }
-
-        this.previousTouchY = currentTouchY; // Update previous touch position
-      }
-    );
-  }
-}
+export class OnboardingComponent {}
